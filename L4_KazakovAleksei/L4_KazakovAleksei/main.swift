5e5a5b21ef507203b3d9ba8039effc5c5a65a579
CristopherJohnson <henkok127@mail.ru>
2018-12-02 20:32:58 +0300

    сделал дз

---
+++ L4_KazakovAleksei/L4_KazakovAleksei/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L4_KazakovAleksei
+//
+//  Created by Алексей Казаков on 02/12/2018.
+//  Copyright © 2018 Алексей Казаков. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
